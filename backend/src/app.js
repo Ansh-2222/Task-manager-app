925838694fbeaf9e42aec971a01970f4be37c65f
Ansh <sharmansh12@gmail.com>
2025-11-20 02:06:41 +0530

    removed cors value

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-app.use(cors({ origin: "task-manager-app-nine-plum.vercel.app", credentials: true }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
